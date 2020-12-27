cf0ae4b6878efebbb44308b65652655969d3ca8c
Toni <aamengual@cifpfbmoll.eu>
2020-12-27 17:31:30 +0100

    release: versio 1.1

--- app/server.js
+++ app/server.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hola mon!'); //write a response to the client
+  res.write('Hola mon! zz'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 8080doc
